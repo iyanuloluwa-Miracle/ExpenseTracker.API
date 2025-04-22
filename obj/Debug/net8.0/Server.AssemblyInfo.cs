01398a54e021b05ebd9bf3a2755a5abc53c90d94
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-04-22 20:08:13 +0100

    Add initial .gitignore and Server solution file

--- obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55bff5b0bd522e2f30d767ca50ccb965ffb5a79b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3ac2a79f387e4cd7ce3c2c8824e2e3577d0aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
